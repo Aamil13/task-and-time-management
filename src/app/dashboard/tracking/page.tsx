cec2f8e48b76295f472baa9f7d7c772131f298d1
Aamil13 <aamil.shafi13@gmail.com>
2026-09-18 23:46:53 +0530

    deployment fix

--- src/app/dashboard/tracking/page.tsx
+++ src/app/dashboard/tracking/page.tsx
@@ -1,13 +1,21 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import { TaskLogsView } from "@/components/organisms/TaskLogView";
 import { AllLogsView } from "@/components/organisms/allLogsView";
 
-
-export default function TrackingPage() {
+function TrackingContent() {
   const searchParams = useSearchParams();
   const taskId = searchParams.get("taskId");
 
   return taskId ? <TaskLogsView taskId={taskId} /> : <AllLogsView />;
+}
+
+export default function TrackingPage() {
+  return (
+    <Suspense>
+      <TrackingContent />
+    </Suspense>
+  );
 }
